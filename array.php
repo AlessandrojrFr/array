f08e2f7c7f8839f4797ed057a404cd56017fc0e3
ALESSANDRO FERNANDES ROCHA JUNIOR <alessandro.6957@aluno.pr.senac.br>
2023-03-09 11:23:26 -0300

    teste Dieimes2

---
+++ array.php
@@ -0,0 +1,42 @@
+<?php
+   $pessoa1 = array();
+   $pessoa2 = array();
+   $pessoa3 = array();
+   $pessoa4 = array();
+
+   $pessoa1 = [0][0][0][0][0][0] = "Alexandre Fernandes";
+   $pessoa1 = [0][0][0][0][1][1] =  21;
+   $pessoa1 = [0][0][0][0][2][2] = "Americana (SP)";
+
+
+
+
+   $pessoa2 = [0][0][0][0][0][0] =  "Luiz Eduardo Rosaweski ";
+   $pessoa2 = [0][0][0][0][1][1] =  18;
+   $pessoa2 = [0][0][0][0][2][2] =   "São Paulo (SP)";
+  
+
+
+   $pessoa3 = [0][0][0][0][0][0] =  "Matheus Zanoni";
+   $pessoa3 = [0][0][0][0][1][1] =  38;
+   $pessoa3 = [0][0][0][0][2][2] =  "Santos (SP)";
+  
+  
+  
+
+
+   $pessoa4 = [0][0][0][0][0][0] = "João Pedro Talarico";
+   $pessoa4 = [0][0][0][0][1][1] =  25;
+   $pessoa4 = [0][0][0][0][2][2] =  "São Paulo (SP)";
+
+
+   echo " O " . $pessoa1[0][0][0][0][0][0]  . " tem " . $pessoa1[0][0][0][0][1][1] . " e é da casa da " . $pessoa1[0][0][0][0][2][2];
+    echo("</br>");
+    echo " O " . $pessoa2[0][0][0][0][0][0] . " tem " . $pessoa2[0][0][0][0][1][1] . " e é da casa da " . $pessoa2[0][0][0][0][2][2];
+    echo("</br>");
+    echo " O " . $pessoa3[0][0][0][0][0][0] . " tem " . $pessoa3[0][0][0][0][1][1] . " e é da casa da " . $pessoa3[0][0][0][0][2][2];
+    echo("</br>");
+    echo " O " . $pessoa4[0][0][0][0][0][0] . " tem " . $pessoa4[0][0][0][0][1][1] . " e é da casa da " . $pessoa4[0][0][0][0][2][2];
+
+   
+?>
